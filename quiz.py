84e8df07ea1311601bb064fe4f874096a50b4491
Tristan Lea Smith <smithtl1@dcsdk12.org>
2016-09-09 12:37:58 -0600

    Fixed version

--- quiz.py
+++ quiz.py
@@ -1,6 +1,6 @@
 
 def has_teen(a, b, c):
-	if a >= 13 and a <= 19 and b >= 13 and b <= 19 and c >=13 and c<= 19:
+	if a >= 13 and a <= 19 or b >= 13 and b <= 19 or c >=13 and c<= 19:
 		return True
 	else:
 		return False
@@ -9,6 +9,8 @@ print has_teen(13, 15, 17) #True
 print has_teen(13, 15, 8) #False
 print has_teen(2, 4, 6) #False
 print has_teen(13, 13, 15) #True
+print has_teen(19, 1, 4) #True
+print has_teen(19, 13, 4) #True
 
 def not_string(str):
 	if str.startswith ('not'):
@@ -31,9 +33,13 @@ print icy_hot(2, 70) #False
 print icy_hot(4, 101) #False
 print icy_hot(102, -2)#True
 # TODO - write closer_to
-def closer_to(a, b, c):
-	if (a - b) > (a - C):
-		return c
+def closer_to(t, a, b):
+	if abs(t-a) < abs(t-b):
+		return a
+	if abs (t-b) < abs(t-a):
+		return b
+	if abs(t-a) == abs(t-b):
+		return 0
 
 print closer_to(5, 8, 4) #4
 print closer_to(5, 3, 1) #3
